fb60599d2b77bb9122b6025c282feae76ce4c441
PharmCat <v.s.arnautov@yandex.ru>
2021-06-28 02:03:02 +0300

    doc push

--- src/setdosetime.jl
+++ src/setdosetime.jl
@@ -1,29 +1,39 @@
 
-
+"""
+    setdosetime!(data::T, dosetime::DoseTime) where T <: PKSubject
+"""
 function setdosetime!(data::T, dosetime::DoseTime) where T <: PKSubject
     data.dosetime = dosetime
     data
 end
-
+"""
+    setdosetime!(data::DataSet{T}, dosetime::DoseTime, ind::Int) where T <: PKSubject
+"""
 function setdosetime!(data::DataSet{T}, dosetime::DoseTime, ind::Int) where T <: PKSubject
     setdosetime!(data[ind], dosetime)
     data
 end
-
+"""
+    setdosetime!(data::DataSet{T}, dosetime::DoseTime, inds::Union{Vector{Int}, UnitRange{Int}, Tuple{Vararg{Int}}}) where T <: PKSubject
+"""
 function setdosetime!(data::DataSet{T}, dosetime::DoseTime, inds::Union{Vector{Int}, UnitRange{Int}, Tuple{Vararg{Int}}}) where T <: PKSubject
     for i in inds
         setdosetime!(data[i], dosetime)
     end
     data
 end
-
+"""
+    setdosetime!(data::DataSet{T}, dosetime::DoseTime) where T <: PKSubject
+"""
 function setdosetime!(data::DataSet{T}, dosetime::DoseTime) where T <: PKSubject
     for i = 1:length(data)
         setdosetime!(data[i], dosetime)
     end
     data
 end
-
+"""
+    setdosetime!(data::DataSet{T}, dosetime::DoseTime, sort::Dict) where T <: PKSubject
+"""
 function setdosetime!(data::DataSet{T}, dosetime::DoseTime, sort::Dict) where T <: PKSubject
     for i = 1:length(data)
         if sort ⊆ data[i].id setdosetime!(data[i], dosetime) end
