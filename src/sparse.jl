780c468b1d8b36f00a75b260d2afca791297baa2
PharmCat <13901158+PharmCat@users.noreply.github.com>
2022-11-28 12:25:54 +0300

    docs

--- src/sparse.jl
+++ src/sparse.jl
@@ -5,9 +5,17 @@ AUC for sparse data.
 
 ```math
 w_1 = (t_2 - t_1) / 2
-w_j = (t_{j+1} - t_{j-1}) / 2  (2 \\leq j \\leq J - 1)
-w_J = (t_J - t_{J-1}) / 2
+```
 
+```math
+w_j = (t_{j+1} - t_{j-1}) / 2  (2 \\leq j \\leq J - 1)
+```
+
+```math
+w_J = (t_J - t_{J-1}) / 2
+```
+
+```math
 AUC = \\sum_{j=1}^J \\mu_j w_j
 ```
 
