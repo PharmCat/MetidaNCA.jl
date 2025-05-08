548a5a91ae9534331b1fdd3da8eca50f4351162b
PharmCat <13901158+PharmCat@users.noreply.github.com>
2025-05-08 05:34:49 +0300

    fix

--- docs/manual.jl
+++ docs/manual.jl
@@ -2,7 +2,7 @@ weave(joinpath(dirname(@__FILE__), "manual_ru.jmd");
 doctype = "pandoc2pdf",
 
 out_path = joinpath(dirname(@__FILE__), "src"),
-pandoc_options=["--toc", "-V colorlinks=true" , "-V linkcolor=blue", "-V urlcolor=red", "-V lang=ru", "-V babel-lang=russian",
+pandoc_options=["--toc", "-V colorlinks=true" , "-V linkcolor=blue", "-V urlcolor=red", "-V lang=ru", "-V babel-lang=russian", "-V mainfont=Arial",
 "-V toccolor=gray", "--number-sections"])
 
 rm(joinpath(dirname(@__FILE__), "src", "manual_ru.aux"); force=true)
