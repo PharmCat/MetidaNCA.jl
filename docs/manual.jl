55b1e560ce249b178bc61cdfb0f9aa1ba6b5c4df
PharmCat <13901158+PharmCat@users.noreply.github.com>
2025-05-09 20:19:01 +0300

    uod

--- docs/manual.jl
+++ docs/manual.jl
@@ -2,7 +2,7 @@ weave(joinpath(dirname(@__FILE__), "manual_ru.jmd");
 doctype = "pandoc2pdf",
 out_path = joinpath(dirname(@__FILE__), "src"),
 pandoc_options=["--toc", "--pdf-engine=xelatex", "-V colorlinks=true" , "-V linkcolor=blue", "-V urlcolor=red", "-V lang=ru", "-V babel-lang=russian", 
- "-V mainfont=\"DejaVu Sans\"", "-V toccolor=gray", "--number-sections"])
+ "-V mainfont=DejaVuSans", "-V toccolor=gray", "--number-sections"])
 
 rm(joinpath(dirname(@__FILE__), "src", "manual_ru.aux"); force=true)
 rm(joinpath(dirname(@__FILE__), "src", "manual_ru.log"); force=true)
